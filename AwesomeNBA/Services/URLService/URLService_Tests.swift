94c2e713f88651be06f7b504cfdeb9ae5b542c1b
TonyJem <62282734+TonyJem@users.noreply.github.com>
2024-01-24 09:49:51 +0200

    Add more tests for URLService

--- AwesomeNBA/Services/URLService/URLService_Tests.swift
+++ AwesomeNBA/Services/URLService/URLService_Tests.swift
@@ -26,7 +26,27 @@ class URLService_Tests: XCTestCase {
         XCTAssertEqual(components.host, "www.balldontlie.io")
         XCTAssertEqual(components.path, "/api/v1/teams")
         XCTAssertEqual(components.queryItems, [item1, item2])
+    }
+    
+    func test_URLService_createURLComponents_With_getGamesEndpoint_allComponentsShouldBeCorrect() {
+        // Given
+        let teamId = Int.random(in: 1...999)
+        let page = Int.random(in: 1...99)
         
+        let endPoint = EndPoint.getGames(teamId: teamId, page: page)
+        let urlService = URLService()
+        
+        let item1 = URLQueryItem(name: "team_ids[]", value: String(teamId))
+        let item2 = URLQueryItem(name: "page", value: String(page))
+        
+        // When
+        let components = urlService.createURLComponents(endpoint: endPoint)
+        
+        // Then
+        XCTAssertEqual(components.scheme, "https")
+        XCTAssertEqual(components.host, "www.balldontlie.io")
+        XCTAssertEqual(components.path, "/api/v1/games")
+        XCTAssertEqual(components.queryItems, [item1, item2])
     }
 
 }
