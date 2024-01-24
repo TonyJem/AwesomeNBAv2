c5a659a7e35c08160f5d7989b8131817683b2959
TonyJem <62282734+TonyJem@users.noreply.github.com>
2024-01-24 09:08:35 +0200

    Create URLService_Tests

---
+++ AwesomeNBA/Services/URLService/URLService_Tests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import AwesomeNBA
+
+class URLService_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testEmptyTest() {
+        
+    }
+
+
+}
