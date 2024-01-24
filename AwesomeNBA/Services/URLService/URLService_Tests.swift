f71b58e109037f7d3503744d02e03dc77b9cdd1e
TonyJem <62282734+TonyJem@users.noreply.github.com>
2024-01-24 09:42:43 +0200

    Start creating tests

--- AwesomeNBA/Services/URLService/URLService_Tests.swift
+++ AwesomeNBA/Services/URLService/URLService_Tests.swift
@@ -9,9 +9,24 @@ class URLService_Tests: XCTestCase {
     override func tearDownWithError() throws {
     }
 
-    func testEmptyTest() {
+    
+    func test_URLService_createURLComponents_With_getTeamsEndpoint_allComponentsShouldBeCorrect() {
+        // Given
+        let endPoint = EndPoint.getTeams
+        let urlService = URLService()
+        
+        let item1 = URLQueryItem(name: "page", value: "1")
+        let item2 = URLQueryItem(name: "per_page", value: "100")
+        
+        // When
+        let components = urlService.createURLComponents(endpoint: endPoint)
+        
+        // Then
+        XCTAssertEqual(components.scheme, "https")
+        XCTAssertEqual(components.host, "www.balldontlie.io")
+        XCTAssertEqual(components.path, "/api/v1/teams")
+        XCTAssertEqual(components.queryItems, [item1, item2])
         
     }
-
 
 }
