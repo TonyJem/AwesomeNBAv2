1333c4287bb7c162c6f29a4142c4e99840c9f6e3
TonyJem <62282734+TonyJem@users.noreply.github.com>
2024-01-24 09:53:32 +0200

    Update URLService_Tests.swift

--- AwesomeNBA/Services/URLService/URLService_Tests.swift
+++ AwesomeNBA/Services/URLService/URLService_Tests.swift
@@ -3,17 +3,20 @@ import XCTest
 
 class URLService_Tests: XCTestCase {
 
+    var urlService: URLServiceProtocol!
+
     override func setUpWithError() throws {
+        urlService = URLService()
     }
 
     override func tearDownWithError() throws {
+        urlService = nil
     }
 
     
     func test_URLService_createURLComponents_With_getTeamsEndpoint_allComponentsShouldBeCorrect() {
         // Given
         let endPoint = EndPoint.getTeams
-        let urlService = URLService()
         
         let item1 = URLQueryItem(name: "page", value: "1")
         let item2 = URLQueryItem(name: "per_page", value: "100")
@@ -33,13 +36,12 @@ class URLService_Tests: XCTestCase {
         let teamId = Int.random(in: 1...999)
         let page = Int.random(in: 1...99)
         
+        // When
         let endPoint = EndPoint.getGames(teamId: teamId, page: page)
-        let urlService = URLService()
         
         let item1 = URLQueryItem(name: "team_ids[]", value: String(teamId))
         let item2 = URLQueryItem(name: "page", value: String(page))
         
-        // When
         let components = urlService.createURLComponents(endpoint: endPoint)
         
         // Then
