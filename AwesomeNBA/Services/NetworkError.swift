9c5cca95ee0debbdab3e08e18bb5c4368e5675fd
TonyJem <62282734+TonyJem@users.noreply.github.com>
2024-01-28 13:25:48 +0200

    Create description for NetworkError

--- AwesomeNBA/Services/NetworkError.swift
+++ AwesomeNBA/Services/NetworkError.swift
@@ -1,9 +1,28 @@
 import Foundation
 
 enum NetworkError: Error {
-    case badUrl
+    
+    // TODO: Check if all error are in use
+    // Check naming and proper description for each error
+    // Should we add more or there are some redundant errors?
+    case invalidUrl
     case invalidRequest
     case badResponse
-    case badStatus
+    case wrongStatus
     case failedToDecodeResponse
+    
+    var description: String {
+        switch self {
+        case .invalidUrl:
+            return "🔴 There was an error creating the URL"
+        case .invalidRequest:
+            return "🔴🔴 invalidRequest"
+        case .badResponse:
+            return "🔴🔴 Did not get a valid response"
+        case .wrongStatus:
+            return "🔴🔴🔴 Did not get a 2xx status code from the response"
+        case .failedToDecodeResponse:
+            return "🔴🔴🔴🔴 Failed to decode response into the given type"
+        }
+    }
 }
--- AwesomeNBA/Services/NetworkService.swift
+++ AwesomeNBA/Services/NetworkService.swift
@@ -10,18 +10,18 @@ final class NetworkService: NetworkServiceProtocol {
     
     func fetchData<T: Codable>(components: URLComponents) async -> T? {
         do {
-            guard let url = components.url else { throw NetworkError.badUrl }
+            guard let url = components.url else { throw NetworkError.invalidUrl }
             let (data, response) = try await URLSession.shared.data(from: url)
             guard let response = response as? HTTPURLResponse else { throw NetworkError.badResponse }
-            guard response.statusCode >= 200 && response.statusCode < 300 else { throw NetworkError.badStatus }
+            guard response.statusCode >= 200 && response.statusCode < 300 else { throw NetworkError.wrongStatus }
             guard let decodedResponse = try? JSONDecoder().decode(T.self, from: data) else { throw NetworkError.failedToDecodeResponse }
             
             return decodedResponse
-        } catch NetworkError.badUrl {
+        } catch NetworkError.invalidUrl {
             print("🔴 There was an error creating the URL")
         } catch NetworkError.badResponse {
             print("🔴🔴 Did not get a valid response")
-        } catch NetworkError.badStatus {
+        } catch NetworkError.wrongStatus {
             print("🔴🔴🔴 Did not get a 2xx status code from the response")
         } catch NetworkError.failedToDecodeResponse {
             print("🔴🔴🔴🔴 Failed to decode response into the given type")
